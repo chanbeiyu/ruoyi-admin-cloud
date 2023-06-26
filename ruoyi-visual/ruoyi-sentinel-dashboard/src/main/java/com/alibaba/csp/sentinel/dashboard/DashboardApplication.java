cc824b4ba24368f27593b60b6309f2b32648daec
chpeng <chpeng@outlook.com>
2023-06-26 23:56:03 +0800

    合并 2.X 主分支（2023/06/55） https://gitee.com/dromara/RuoYi-Cloud-Plus/tree/2.X/

--- ruoyi-visual/ruoyi-sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/DashboardApplication.java
+++ ruoyi-visual/ruoyi-sentinel-dashboard/src/main/java/com/alibaba/csp/sentinel/dashboard/DashboardApplication.java
@@ -26,11 +26,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Carpenter Lee
  */
 @SpringBootApplication
-public class SentinelApplication {
+public class DashboardApplication {
 
     public static void main(String[] args) {
         triggerSentinelInit();
-        SpringApplication.run(SentinelApplication.class, args);
+        SpringApplication.run(DashboardApplication.class, args);
     }
 
     private static void triggerSentinelInit() {
