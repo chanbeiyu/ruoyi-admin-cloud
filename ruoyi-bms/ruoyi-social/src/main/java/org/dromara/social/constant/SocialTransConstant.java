e42a32909eb7f508660abc4da2d4e2171313e607
chpeng <chpeng@outlook.com>
2023-06-29 10:49:51 +0800

    app信息翻译

--- ruoyi-bms/ruoyi-social/src/main/java/org/dromara/social/constant/SocialTransConstant.java
+++ ruoyi-bms/ruoyi-social/src/main/java/org/dromara/social/constant/SocialTransConstant.java
@@ -9,8 +9,6 @@ public interface SocialTransConstant {
 
     String SOCIAL_ID_TO_NAME = "app_id_to_name";
 
-    String SOCIAL_CODE_TO_NAME = "app_code_to_name";
-
     interface Other {
 
         String APP = "app";
---
+++ ruoyi-bms/ruoyi-social/src/main/java/org/dromara/social/translation/SocialTranslation.java
@@ -0,0 +1,45 @@
+package org.dromara.social.translation;
+
+import lombok.AllArgsConstructor;
+import org.dromara.common.translation.annotation.TranslationType;
+import org.dromara.common.translation.core.TranslationInterface;
+import org.dromara.social.constant.SocialTransConstant;
+import org.dromara.social.service.ISocialAppService;
+import org.dromara.social.service.ISocialNoticeTypeService;
+import org.dromara.social.service.ISocialSubjectService;
+import org.dromara.social.service.ISocialTagService;
+
+/**
+ * @author wy_peng_chen6
+ */
+@AllArgsConstructor
+@TranslationType(type = SocialTransConstant.SOCIAL_ID_TO_NAME)
+public class SocialTranslation implements TranslationInterface<String> {
+
+    private ISocialAppService socialAppService;
+    private ISocialSubjectService socialSubjectService;
+    private ISocialNoticeTypeService socialNoticeTypeService;
+    private ISocialTagService socialTagService;
+
+    @Override
+    public String translation(Object key, String other) {
+        switch (other) {
+            case SocialTransConstant.Other.APP -> {
+                return socialAppService.queryById(Long.parseLong(key.toString())).getAppName();
+            }
+            case SocialTransConstant.Other.NOTICE -> {
+                return socialNoticeTypeService.queryById(Long.parseLong(key.toString())).getNoticeTypeName();
+            }
+            case SocialTransConstant.Other.TAG -> {
+                return socialTagService.queryById(Long.parseLong(key.toString())).getTagName();
+            }
+            case SocialTransConstant.Other.SUBJECT -> {
+                return socialSubjectService.queryById(Long.parseLong(key.toString())).getSocialName();
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
+
+}
