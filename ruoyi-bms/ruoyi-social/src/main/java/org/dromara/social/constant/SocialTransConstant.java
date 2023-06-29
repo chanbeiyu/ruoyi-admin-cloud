cbef131595f92d13784693f2ee49046c874de37a
chpeng <chpeng@outlook.com>
2023-06-29 10:48:59 +0800

    app信息翻译

---
+++ ruoyi-bms/ruoyi-social/src/main/java/org/dromara/social/constant/SocialTransConstant.java
@@ -0,0 +1,23 @@
+package org.dromara.social.constant;
+
+/**
+ * 翻译常量
+ *
+ * @author Lion Li
+ */
+public interface SocialTransConstant {
+
+    String SOCIAL_ID_TO_NAME = "app_id_to_name";
+
+    String SOCIAL_CODE_TO_NAME = "app_code_to_name";
+
+    interface Other {
+
+        String APP = "app";
+        String SUBJECT = "subject";
+        String NOTICE = "notice";
+        String TAG = "tag";
+
+    }
+
+}
